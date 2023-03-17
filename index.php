448cb60e72bd2f77266c9ab6b84b8f07ec4e5f6b
Khiro Sasha <sasha.khiro@gmail.com>
2023-03-17 23:36:30 +0200

    connect bootstrap

--- index.php
+++ index.php
@@ -17,8 +17,6 @@ if (isset($_POST["submit"])) {
 
     foreach ($urlArray as $url) {
         $xml = simplexml_load_file($url);
-
-
         array_push($rss, $xml);
     }
 }
@@ -33,13 +31,15 @@ if (isset($_POST["submit"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rss Feed Reader</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
     <h4>URL</h4>
     <form method="post">
         <label for="urls">Введите URL-адреса через запятую:</label>
-        <textarea id="urls" name="urls" rows="5" cols="50"></textarea>
+        <textarea id="urls" name="urls"></textarea>
         <input type="submit" name="submit" value="Отправить">
     </form>
 
@@ -60,6 +60,10 @@ if (isset($_POST["submit"])) {
             <?php endforeach ?>
         </div>
     <?php endif; ?>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
