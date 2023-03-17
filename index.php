0fba905860715cded7dae8d818e99dd4fd39b6ca
Khiro Sasha <sasha.khiro@gmail.com>
2023-03-17 18:43:00 +0200

    change form and get data

--- index.php
+++ index.php
@@ -1,26 +1,31 @@
 <?php
 libxml_use_internal_errors(TRUE);
 
-$rss = [];
+static $rss = [];
 $err = "";
+$urlArray = [];
 
 if (isset($_POST["submit"])) {
-    $xml = simplexml_load_file($_POST['rssWeb']);
 
-    if ($xml === FALSE) {
-        $err = "rss not available";
-    } else {
-        $rss = $xml;
-    }
+    $urls = $_POST['urls'];
+
+    $delimiter = '/[\n\s]+/';
+
+    $urlArray = preg_split($delimiter, $urls);
+    $urlArray = array_map('trim', $urlArray);
+    $urlArray = array_filter($urlArray);
+    
 }
 
-function addPosts($array){
-    echo "<h2>".$array->channel->title."</h2>";
-    foreach($array->channel->item as $item){
-        echo "<div><p><a href='".$item->link."'>".$item->title."</a></p>";
-        echo "<p>".$item->description."</p>";
-        echo "<img src='".$item->image->url."' alt='".$item->image->title."'></div>";
-    }
+function addPosts()
+{
+    /*echo "<h2>" . $->channel->title . "</h2>";
+    foreach($rss as $array)
+    foreach ($array->channel->item as $item) {
+        echo "<div><p><a href='" . $item->link . "'>" . $item->title . "</a></p>";
+        echo "<p>" . $item->description . "</p>";
+        echo "<img src='" . $item->image->url . "' alt='" . $item->image->title . "'></div>";
+    }*/
 }
 ?>
 
@@ -32,23 +37,22 @@ function addPosts($array){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rss Reader</title>
+    <title>Rss Feed Reader</title>
 </head>
 
 <body>
     <h4>URL</h4>
     <form method="post">
-        <input type="url" name="rssWeb">
-        <input type="submit" name="submit" value="Add">
+        <label for="urls">Введите URL-адреса через запятую:</label>
+        <textarea id="urls" name="urls" rows="5" cols="50"></textarea>
+        <input type="submit" name="submit" value="Отправить">
     </form>
 
     <?php
-        if(strlen($err) != 0) {
-            echo '<h2>'. $err . '</h2>';	
-        }
-        elseif (count($rss)!=0){
-            addPosts($rss);
-        }
+    echo count($urlArray);
+    if (count($urlArray) > 0) {
+        print_r($urlArray);
+    }
     ?>
 </body>
 
